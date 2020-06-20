6afa9e1a85debbf1868c758b11f7c1b3f686abdb
Yann Kerveant <53348914+ykerveant@users.noreply.github.com>
2020-06-20 08:37:05 -0400

    Update code.py

--- code.py
+++ code.py
@@ -31,23 +31,29 @@ print(df1.columns)
 frames = [df0, df1]
 # when in production, replace frames for create_list... function
 master_df = pd.concat(frames, ignore_index=True)
+master_df.sort_values(by='Id', inplace=True)
 
+# end of master_df
 print(master_df)
 print(master_df.columns)
 
+# creating sf dataFrame for color formating
 sf = StyleFrame(master_df)
+cols_to_style = [cols for cols in sf.columns]
+
 # applying color for student with 1
 sf.apply_style_by_indexes(indexes_to_style=sf[(sf['note'] <= 1)],
-                          cols_to_style=['Nom', 'Prenom', 'note'],
+                          cols_to_style=cols_to_style,
                           styler_obj=Styler(bg_color='green', bold=True, font_size=10))
+
 # applying color for student with 2
 sf.apply_style_by_indexes(indexes_to_style=sf[(sf['note'] > 1) & (sf['note'] < 3)],
-                          cols_to_style=['Nom', 'Prenom', 'note'],
+                          cols_to_style=cols_to_style,
                           styler_obj=Styler(bg_color='yellow', bold=True, font_size=10))
 
 # applying color for student with 3
 sf.apply_style_by_indexes(indexes_to_style=sf[(sf['note'] >= 3)],
-                          cols_to_style=['Nom', 'Prenom', 'note'],
+                          cols_to_style=cols_to_style,
                           styler_obj=Styler(bg_color='red', bold=True, font_size=10))
 
 sf.to_excel('master_df.xlsx').save()
